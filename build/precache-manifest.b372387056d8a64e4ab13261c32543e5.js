59b30d0f9d2407e29cc5f211d1fa75ef99fd7d73
Tahzib <tahzibsunesara1@gmail.com>
2020-05-06 10:19:04 -0500

    updated changes

---
+++ build/precache-manifest.b372387056d8a64e4ab13261c32543e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0595b16d5a10a85d453a008b7c92acf",
+    "url": "tahzib123.github.io/NbaApp/index.html"
+  },
+  {
+    "revision": "703c99c64fee2073c92f",
+    "url": "tahzib123.github.io/NbaApp/static/css/main.a4e0f9a6.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "tahzib123.github.io/NbaApp/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "703c99c64fee2073c92f",
+    "url": "tahzib123.github.io/NbaApp/static/js/main.def496d6.chunk.js"
+  },
+  {
+    "revision": "eab9ae3df784fdbef8fe",
+    "url": "tahzib123.github.io/NbaApp/static/js/runtime~main.c7038aae.js"
+  }
+]);
